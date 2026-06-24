1aaf2df42fd56c6ba4be44cab790a23231cc4223
Advay Welling <advaywelling@gmail.com>
2026-06-23 23:35:11 -0500

    more

--- chip8.cc
+++ chip8.cc
@@ -1,4 +1,7 @@
 #include "chip8.h"
+#include <fstream>
+#include <iostream> 
+#include <cstring> 
 
 chip8::chip8() {
     pc = 0x200;
@@ -26,5 +29,68 @@ chip8::chip8() {
 }
 
 bool chip8::load_rom(const std::string& filename) {
-    
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << '\n';
+        return false;
+    }
+    std::streamsize size = file.tellg();
+    if ((4096 - 0x200) < size) {
+        std::cerr << filename << " too big\n";
+        return false;
+    }
+    file.seekg(0, std::ios::beg);
+    file.read(reinterpret_cast<char*>(mem + 0x200), size);
+    return true;
+}
+
+void chip8::cycle() {
+    uint16_t opcode = (mem[pc] << 8) | mem[pc + 1];
+    pc += 2;
+    switch (opcode & 0xF000) {
+    case 0x0000:
+        if (opcode == 0x00E0) {
+            std::memset(display, 0, sizeof(display));
+        } else if (opcode == 0x00EE) {
+            sp--;
+            pc = stack[sp];
+        }
+        break;
+    case 0x1000:
+        pc = opcode & 0x0FFF;
+        break;
+    case 0x2000:
+        stack[sp] = pc;
+        sp++;
+        pc = opcode & 0x0FFF;
+        break;
+    case 0x3000:
+        break;
+    case 0x4000:
+        break;
+    case 0x5000:
+        break;
+    case 0x6000:
+        break;
+    case 0x7000:
+        break;
+    case 0x8000:
+        break;
+    case 0x9000:
+        break;
+    case 0xA000:
+        break;
+    case 0xB000:
+        break;
+    case 0xC000:
+        break;
+    case 0xD000:
+        break;
+    case 0xE000:
+        break; 
+    case 0xF000:
+        break;
+    default:
+        break;
+    }
 }
